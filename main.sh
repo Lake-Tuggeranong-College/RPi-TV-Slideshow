9c0f9119defd633b1adb955f33606584aad6133e
Dylan <40590952+TheDjgamerProductions@users.noreply.github.com>
2023-11-02 01:54:47 +0000

    Rewrite now works
    WIll implment generic slideshow functionality later

--- main.sh
+++ main.sh
@@ -1,28 +1,38 @@
-#! /bin/bash/
+#!/bin/bash
+
+waitForNetworkConnection() {
+    while [ "$res" != "200" ]
+    do
+    res=$(curl -Is https://www.google.com | awk '/^HTTP/{print $2}')
+    echo $res
+	done
+}
+
 
 waitForNetworkConnection
 
-# Update and Install packags
-sudo apt update
 
 HOSTNAME=$(hostname)
 
-# Retrave MACADDRESS List from the github repo
+echo $HOSTNAME
+
+# Retrave URL HostName Key Value Pair List from the github repo
 LIST=$(curl https://raw.githubusercontent.com/Lake-Tuggeranong-College/RPi-TV-Slideshow/2023-Rewrite/SLIDESHOWLIST.json)
 
-SLIESHOWURL=$(jq '.$HOSTNAME.Slideshow' LIST)
+echo $LIST
 
+jqFilter=$(echo .$HOSTNAME.Slideshow)
 
-chromium-browser $SLIESHOWURL --kiosk --incongnito
+echo $jqFilter
 
+SLIDESHOWURL=$(echo $LIST | jq $jqFilter)
 
+echo $SLIDESHOWURL
 
-function waitForNetworkConnection () {
-    while [ "$responce" != "200" ]
-    do
-    res=$(curl -Is https://www.google.com | awk '/^HTTP/{print $2}')
-    done
-}
+SLIDESHOWURL=${SLIDESHOWURL/'"'/}
+SLIDESHOWURL=${SLIDESHOWURL/'"'/}
+
+chromium-browser $SLIDESHOWURL --kiosk --incongnito
 
 
 
