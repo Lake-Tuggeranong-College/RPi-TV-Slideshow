61701f6c340a545969e0ab50df6450b916639eb1
Dylan <40590952+TheDjgamerProductions@users.noreply.github.com>
2023-11-16 22:34:16 +0000

    Script now gets and sets date and time
    Due to shared services blocking NTP and not allowing us to use their NTP server, i made my own way to get time and date

--- main.sh
+++ main.sh
@@ -13,6 +13,12 @@ waitForNetworkConnection() {
 waitForNetworkConnection
 
 
+sudo timedatectl set-ntp off
+dateTimeAPIResponse=$(curl "http://worldtimeapi.org/api/timezone/Australia/Sydney")
+unixTimeNoOffset=$(echo $dateTimeAPIResponse | jq .unixtime)
+unixtime=$((unixTimeNoOffset+39600))
+sudo date -s @$unixtime
+
 HOSTNAME=$(hostname)
 
 echo $HOSTNAME
@@ -39,7 +45,6 @@ SLIDESHOWURL=${SLIDESHOWURL/'"'/}
 SLIDESHOWURL=${SLIDESHOWURL/'"'/}
 
 chromium-browser $SLIDESHOWURL --kiosk --incongnito
-
 
 
 
